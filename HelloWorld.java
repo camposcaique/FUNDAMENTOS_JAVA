d569cf5f14878344110d50728e81d4fc53ca483d
camposcaique <camppos.caique@gmail.com>
2023-11-16 13:14:25 -0300

    Tipos de dados

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,19 @@
 public class HelloWorld {
 
     // Todo conteúdo deverá ser inderido aqui dentro.
+    public static void main(String[] args) {
 
+        /* Valores (int, double, float long)
+         * Texto (String)
+         * Booleanos (boolean)
+         */
+        int DadoDoTipoInt = 10;
+        double DadoDoTipoDouble = 3.14;
+        float DadoDoTipoFloat = 3.14f;
+        long DadoDoTipoLong = 4372894037289l;
+        String DadoDoTipoString = "Colocar o meu texto";
+        Boolean DadoDoTipoBoolean = false;
+    }
     
 }
+// Fora do escopo da classe
