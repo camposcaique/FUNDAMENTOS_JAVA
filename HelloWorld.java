4d3df0197507184b3497d38582d7e6cd07fb8a82
camposcaique <camppos.caique@gmail.com>
2023-11-16 13:36:44 -0300

    Fim da estrutura de repetição

--- HelloWorld.java
+++ HelloWorld.java
@@ -13,12 +13,40 @@ public class HelloWorld {
          * Texto (String)
          * Booleanos (boolean)
          */
-        int DadoDoTipoInt = 10;
-        double DadoDoTipoDouble = 3.14;
-        float DadoDoTipoFloat = 3.14f;
-        long DadoDoTipoLong = 4372894037289l;
-        String DadoDoTipoString = "Colocar o meu texto";
-        Boolean DadoDoTipoBoolean = false;
+        int dadoDoTipoInt = 10;
+        double dadoDoTipoDouble = 3.14;
+        float dadoDoTipoFloat = 3.14f;
+        long dadoDoTipoLong = 4372894037289l;
+        String dadoDoTipoString = "Colocar o meu texto";
+        Boolean dadoDoTipoBoolean = false;
+
+        // if - else 
+        // if(dadoDoTipoInt == 10) {
+        //     // sysout
+        //     System.out.println("Entrou no if do 10");
+        // } if (dadoDoTipoInt < 12) {
+        //     System.out.println("Entrou no if do 12");
+        // }
+
+        // else {
+        //     System.out.println("Entrou no else");
+        // }
+
+        // While (Enquanto algo for verdadeiro, faça alguma coisa)
+        int valorInicial = 0;
+        while (valorInicial <3) {
+            System.out.println("O valor incial é menor que 3");
+            System.out.println(valorInicial);
+            valorInicial ++; // valorInicial = 3
+        }
+        System.out.println("Saiu do while");
+
+        // For
+        for(int i = 0; i < 4; i++) {
+            System.out.println("O valor do i é: " + is);
+        }
+        System.out.println("Finalizando o FOR");
+
     }
     
 }
