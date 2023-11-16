76f8c9e5834e711e5e7b8e7daf73399f40675ab1
camposcaique <camppos.caique@gmail.com>
2023-11-16 13:02:26 -0300

    Iniciando curso Java da RockeatSeat

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+
+/*
+ * HelloWorld = Nome da minha classe
+ * public = Tipo de acesso da classe
+ * class = Tipo de classe
+ */
+public class HelloWorld {
+
+    // Todo conteúdo deverá ser inderido aqui dentro.
+
+    
+}
