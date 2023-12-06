dc25614f39528ece5f0325f0bec3c15eb77ee50d
camposcaique <camppos.caique@gmail.com>
2023-12-06 14:28:46 -0300

    Aula de Poliformismo

--- pessoa/Aluno.java
+++ pessoa/Aluno.java
@@ -11,6 +11,11 @@ public class Aluno extends Pessoa {
 
     public void setMatricula(String matricula) {
         this.matricula = matricula;
+    
+    }
+        String imprimirDadosDaPessoa() {
+        System.out.println(super.imprimirDadosDaPessoa());
+        return "Você é aluno";
     }
 
 
